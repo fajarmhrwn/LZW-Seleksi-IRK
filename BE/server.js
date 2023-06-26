3dbeca1ad46f99f2e285d24f4b1d9ecea0a86079
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-06-26 14:31:33 +0700

    oke

--- BE/server.js
+++ BE/server.js
@@ -63,7 +63,7 @@ const getData = async (email) => {
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
